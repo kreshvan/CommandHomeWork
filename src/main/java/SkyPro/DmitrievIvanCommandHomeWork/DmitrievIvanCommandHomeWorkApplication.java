3537ea4e4f982f9ceb4905057775e218c2fb985e
KURS HOMEWORK <kreshvan12317@yandex.ru>
2025-01-24 21:59:54 +0300

    ветка

---
+++ src/main/java/SkyPro/DmitrievIvanCommandHomeWork/DmitrievIvanCommandHomeWorkApplication.java
@@ -0,0 +1,14 @@
+package SkyPro.DmitrievIvanCommandHomeWork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmitrievIvanCommandHomeWorkApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(DmitrievIvanCommandHomeWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/SkyPro/DmitrievIvanCommandHomeWork/DmitrievIvanCommandHomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package SkyPro.DmitrievIvanCommandHomeWork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DmitrievIvanCommandHomeWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
